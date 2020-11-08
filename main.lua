b078d0af751b6fdd3bc4be194769fc9d9cb10be5
Dutch Larsen <dutchlar2007@gmail.com>
2020-11-07 21:00:56 -0700

    I switch from using a for separate block spawning system to a single block spawn with the rest based on that block. I also made a function to tell if there are blocks below the current one.

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local dtotal = 0
 if unpack == nil then
   unpack = table.unpack
 end
+
 --=============================================================================
 local colors = {                        -- The format for items is {R,G,B, 'Color'}
   {255,0,0,1},     -- Red 
@@ -9,7 +10,7 @@ local colors = {                        -- The format for items is {R,G,B, 'Colo
   {0,0,255,1},     -- Blue
   {255,255,0,1},   -- Yellow
   {255,0,255,1},   -- Purple
-  {255,255,255,1}, -- White
+  {255,165,0,1},   -- Orange
   {0,255,255,1},   -- Aqua 
 }
 colors[0] = {0,0,0, 1} -- Black
@@ -42,6 +43,13 @@ local map = {
 }
 
 ---------------------------------------------------------------
+function map:isFilled(x,y)
+  if y < 1 or #map < y then return false end
+  if x < 1 or #map[1] < x then return false end
+  return map[y] and map[y][x] and map[y][x] ~= 0
+end
+
+---------------------------------------------------------------
 function map:draw()
   for Y = 1 , #self do 
     for X = 1, #self[Y] do
@@ -54,42 +62,63 @@ end
 --=============================================================================
 local score = {points = 0, speed = 1}
 --=============================================================================
-local block = {dtotal = 0, x = {5,1,1,1}, y = {1,1,1,1},color=7}
+local block = {dtotal = 0, x = 5, y = 1, dx  = {}, dy = {}, reset = 1, color=7}
 
 function block:update(dt)
   self.dtotal = self.dtotal + dt
   if self.dtotal >= 1/score.speed then
-    self.y[1] = math.min(20, self.y[1] + 1)
-    self.y[2] = math.min(20, self.y[2] + 1)
-    self.y[3] = math.min(20, self.y[3] + 1)
-    self.y[4] = math.min(20, self.y[4] + 1)
+    self.y = math.min(20, self.y + 1)
     self.dtotal = 0
     return dtotal
   end
+end 
+
+---------------------------------------------------------------
+
+function block:getX(i)
+  return self.x + self.dx[i]
 end
 
 ---------------------------------------------------------------
 
-function block:moveBlock(key)
-  if key == 'left' and block.y < 20 then
-    block.x[1] = math.max(1, block.x[1] - 1)
-    block.x[2] = math.max(1, block.x[2] - 1)
-    block.x[3] = math.max(1, block.x[3] - 1)
-    block.x[4] = math.max(1, block.x[4] - 1)
-    
+function block:getY(i)
+  return self.y + self.dy[i]
+end
+
+---------------------------------------------------------------
+
+function block:collision()
+  for i = 1, 4 do
+    if self:getY(i) >= 20 then 
+      self.reset = true
+      return true
+    end
+    for i = 1, 4 do
+      local y, x = self:getY(i) + 1, self:getX(i)
+      if map:isFilled(x,y) then
+        self.reset = true 
+        return true
+      end
+    end
   end
-end
-
----------------------------------------------------------------
-function block:formation()
-  if self.color == 1 then
-    
-  end
+  
+  return false
 end
 
 ---------------------------------------------------------------
 
-function block:clockwize(x,y)
+function block:setBlock()
+  for i = 1, 4 do
+    local y, x = self:getY(i), self:getX(i)
+    if not map:isFilled(x, y) then
+      map[y][x] = self.color
+    end
+  end 
+end
+
+---------------------------------------------------------------
+
+function block:clockwise(x,y)
   return -y, x
 end
 
@@ -97,27 +126,56 @@ end
 
 function block:draw()
   local size = map.size
-  love.graphics.setColor(unpack(colors[self.color]))
-  for i = 1, #self.x do
-    love.graphics.rectangle("fill", self.x[i]*size+200, self.y[i]*size-30, size, size)
+  if block:collision() then 
+    block:setBlock()
   end
-  block:setBlock()
+  if self.reset then
+    block:formation()
+  end
+  love.graphics.setColor(unpack(colors[self.color]))
+  for i = 1, 4 do
+    love.graphics.rectangle("fill", self:getX(i)*size+200, self:getY(i)*size-30, size, size)
+  end
 end
+  
+---------------------------------------------------------------
+function block:formation()
+  self.color = math.random(1,7)
+  if self.color == 1 then
+    self.dx = {0,-1,0,1}
+    self.dy = {0,0,-1,-1}
+  end
+  if self.color == 2 then
+    self.dx = {0,-1,0,1}
+    self.dy = {0,-1,-1,0}
+  end
+  if self.color == 3 then
+    self.dx = {0,-1,0,0}
+    self.dy = {0,0,-1,-2}
+  end
+  if self.color == 4 then
+    self.dx = {0,1,0,1}
+    self.dy = {0,0,-1,-1}
+  end
+  if self.color == 5 then
+    self.dx = {0,-1,1,0}
+    self.dy = {0,0,0,-1}
+  end
+  if self.color == 6 then
+    self.dx = {0,1,0,0}
+    self.dy = {0,0,-1,-2}
+  end
+  if self.color == 7 then
+    self.dx = {0,0,0,0}
+    self.dy = {0,-1,-2,-3}
+  end
+  self.x = 5
+  self.y = 0
+  self.reset = false
+end
+block:formation()
 
 ---------------------------------------------------------------
-function block:setBlock()
-  local hitLine20 = false
-  for i = 1, #self.y do
-    if self.y[i] >= 20 then 
-      hitLine20 = true 
-    end
-  end
-  if hitLine20 then
-    for i = 1, #self.y do
-      map[self.y[i]][self.x[i]] = self.color
-    end 
-  end
-end
 
 --=============================================================================
 --=============================================================================
@@ -135,18 +193,28 @@ end
 
 ---------------------------------------------------------------
 function love.keypressed(key)
-  if key == 'left' and block.y[1] and block.y[2] and block.y[3] and block.y[4] < 20 and block.x[1] and block.x[2] and block.x[3] and block.x[4] > 1 then
-    block.x[1] = math.max(1, block.x[1] - 1)
-    block.x[2] = math.max(1, block.x[2] - 1)
-    block.x[3] = math.max(1, block.x[3] - 1)
-    block.x[4] = math.max(1, block.x[4] - 1)
+  if key == 'left' then
+    for i = 1, 4 do
+      local y, x = block:getY(i), block:getX(i) - 1
+      if block:getX(i) <= 1 or map:isFilled(x,y) then
+        goto skip1
+      end
+    end
+    block.x = block.x - 1
+    ::skip1::
   end
-  if key == 'right' and block.y[1] and block.y[2] and block.y[3] and block.y[4] < 20 and block.x[1] and block.x[2] and block.x[3] and block.x[4] < 10 then
-    block.x[1] = math.min(10, block.x[1] + 1)
-    block.x[2] = math.min(10, block.x[2] + 1)
-    block.x[3] = math.min(10, block.x[3] + 1)
-    block.x[4] = math.min(10, block.x[4] + 1)
+  if key == 'right' then
+    for i = 1, 4 do
+      local y, x = block:getY(i), block:getX(i) + 1
+      if block:getX(i) >= 10 or map:isFilled(x,y) then
+        goto skip2
+      end
+    end
+    block.x = block.x + 1
+    ::skip2::
   end
 end
 
-
+function love.load(arg)
+  if arg[#arg] == "-debug" then require("mobdebug").start() end
+end
